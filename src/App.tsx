6b1db0f8b93e54bf5b4516cde7838d717ac138e5
seungkyu2 <zx6658@naver.com>
2018-06-19 14:40:22 +0900

    카운터 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Counter from './components/Counter';
 import Profile from './components/Profile';
 
 
@@ -9,6 +10,7 @@ class App extends React.Component {
         <Profile
         name="이승규"
         job="풀스택"/>
+        <Counter/>
       </div>
     );
   }
